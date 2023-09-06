454283deb1d8a12aaee85dfb1ac5508d9e9b70a5
Yannick Daveluy <yannick.daveluy@gmail.com>
2023-09-06 19:27:11 +0200

    rename the SimSat Profile to ESA CDK Profile

---
+++ org.eclipse.xsmp.profile.esa_cdk/src/org/eclipse/xsmp/profile/esa_cdk/EsaCdkRuntimeModule.java
@@ -0,0 +1,137 @@
+/*******************************************************************************
+* Copyright (C) 2020-2022 THALES ALENIA SPACE FRANCE.
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License 2.0
+* which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+******************************************************************************/
+package org.eclipse.xsmp.profile.esa_cdk;
+
+import org.eclipse.xsmp.XsmpcatExtensionRuntimeModule;
+import org.eclipse.xsmp.generator.cpp.CatalogueGenerator;
+import org.eclipse.xsmp.generator.cpp.CppCopyrightNoticeProvider;
+import org.eclipse.xsmp.generator.cpp.GeneratorUtil;
+import org.eclipse.xsmp.generator.cpp.member.ConstantGenerator;
+import org.eclipse.xsmp.generator.cpp.member.ContainerGenerator;
+import org.eclipse.xsmp.generator.cpp.member.EntryPointGenerator;
+import org.eclipse.xsmp.generator.cpp.member.EventSinkGenerator;
+import org.eclipse.xsmp.generator.cpp.member.EventSourceGenerator;
+import org.eclipse.xsmp.generator.cpp.member.ReferenceGenerator;
+import org.eclipse.xsmp.generator.cpp.type.ArrayGenerator;
+import org.eclipse.xsmp.generator.cpp.type.ComponentGenerator;
+import org.eclipse.xsmp.generator.cpp.type.ExceptionGenerator;
+import org.eclipse.xsmp.generator.cpp.type.StringGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.EsaCdkGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.EsaCdkGeneratorExtension;
+import org.eclipse.xsmp.profile.esa_cdk.generator.EsaCdkOutputConfigurationProvider;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.EsaCdkCatalogueGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.EsaCdkCopyrightProvider;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.member.EsaCdkConstantGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.member.EsaCdkContainerGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.member.EsaCdkEntryPointGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.member.EsaCdkEventSinkGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.member.EsaCdkEventSourceGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.member.EsaCdkReferenceGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.type.EsaCdkArrayGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.type.EsaCdkComponentGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.type.EsaCdkExceptionGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.generator.cpp.type.EsaCdkStringGenerator;
+import org.eclipse.xsmp.profile.esa_cdk.validation.EsaCdkValidator;
+import org.eclipse.xtext.generator.IGenerator2;
+import org.eclipse.xtext.generator.IOutputConfigurationProvider;
+import org.eclipse.xtext.service.SingletonBinding;
+
+/**
+ * Use this class to register components to be used at runtime / without the Equinox extension
+ * registry.
+ */
+public class EsaCdkRuntimeModule extends XsmpcatExtensionRuntimeModule
+{
+
+  @Override
+  public Class< ? extends IGenerator2> bindIGenerator2()
+  {
+    return EsaCdkGenerator.class;
+  }
+
+  @SingletonBinding(eager = true)
+  public Class< ? extends EsaCdkValidator> bindXsmpValidator()
+  {
+    return EsaCdkValidator.class;
+  }
+
+  @Override
+  public Class< ? extends IOutputConfigurationProvider> bindIOutputConfigurationProvider()
+  {
+    return EsaCdkOutputConfigurationProvider.class;
+  }
+
+  public Class< ? extends EntryPointGenerator> bindEntryPointGenerator()
+  {
+    return EsaCdkEntryPointGenerator.class;
+  }
+
+  public Class< ? extends EventSinkGenerator> bindEventSinkGenerator()
+  {
+    return EsaCdkEventSinkGenerator.class;
+  }
+
+  public Class< ? extends EventSourceGenerator> bindEventSourceGenerator()
+  {
+    return EsaCdkEventSourceGenerator.class;
+  }
+
+  public Class< ? extends ReferenceGenerator> bindReferenceGenerator()
+  {
+    return EsaCdkReferenceGenerator.class;
+  }
+
+  public Class< ? extends ContainerGenerator> bindConatinerGenerator()
+  {
+    return EsaCdkContainerGenerator.class;
+  }
+
+  public Class< ? extends ComponentGenerator> bindComponentGenerator()
+  {
+    return EsaCdkComponentGenerator.class;
+  }
+
+  public Class< ? extends CatalogueGenerator> bindCatalogueGenerator()
+  {
+    return EsaCdkCatalogueGenerator.class;
+  }
+
+  public Class< ? extends ArrayGenerator> bindArrayGenerator()
+  {
+    return EsaCdkArrayGenerator.class;
+  }
+
+  public Class< ? extends StringGenerator> bindStringGenerator()
+  {
+    return EsaCdkStringGenerator.class;
+  }
+
+  public Class< ? extends ExceptionGenerator> bindExceptionGenerator()
+  {
+    return EsaCdkExceptionGenerator.class;
+  }
+
+  public Class< ? extends ConstantGenerator> bindConstantGenerator()
+  {
+    return EsaCdkConstantGenerator.class;
+  }
+
+  public Class< ? extends GeneratorUtil> bindGeneratorExtension()
+  {
+    return EsaCdkGeneratorExtension.class;
+  }
+
+  public Class< ? extends CppCopyrightNoticeProvider> bindCopyrightProvider()
+  {
+    return EsaCdkCopyrightProvider.class;
+  }
+
+}
---
+++ org.eclipse.xsmp.profile.esa_cdk/src/org/eclipse/xsmp/profile/esa_cdk/generator/cpp/member/EsaCdkEventSourceGenerator.xtend
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (C) 2020-2022 THALES ALENIA SPACE FRANCE.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+package org.eclipse.xsmp.profile.esa_cdk.generator.cpp.member
+
+import org.eclipse.xsmp.generator.cpp.IncludeAcceptor
+import org.eclipse.xsmp.generator.cpp.member.EventSourceGenerator
+import org.eclipse.xsmp.xcatalogue.EventSource
+import org.eclipse.xsmp.xcatalogue.NamedElementWithMembers
+
+class EsaCdkEventSourceGenerator extends EventSourceGenerator {
+
+    override collectIncludes(EventSource it, IncludeAcceptor acceptor) {
+        if (eventType !== null)
+            acceptor.userHeader("esa/ecss/smp/cdk/EventSourceArg.h")
+        else
+            acceptor.userHeader("esa/ecss/smp/cdk/EventSource.h")
+    }
+
+    override declareGen(NamedElementWithMembers parent, EventSource it, boolean useGenPattern) {
+        val arg = eventType
+        if (arg !== null)
+            '''
+                «comment»
+                ::esa::ecss::smp::cdk::EventSourceArg<«arg.id»> *«name»;
+            '''
+        else
+            '''
+                «comment»
+                ::esa::ecss::smp::cdk::EventSource *«name»;
+            '''
+    }
+
+    override initialize(NamedElementWithMembers parent, EventSource it, boolean useGenPattern) {
+        val arg = eventType
+        if (arg !== null)
+            '''
+                // Event Source: «name»
+                «name»{new ::esa::ecss::smp::cdk::EventSourceArg<«arg.id»>( "«name»", «description()», this, simulator)}
+            '''
+        else
+            '''
+                // Event Source: «name»
+                «name»{new ::esa::ecss::smp::cdk::EventSource( "«name»", «description()», this, simulator)}
+            '''
+
+    }
+
+    override construct(NamedElementWithMembers parent, EventSource it, boolean useGenPattern) {
+        '''
+            // Add event source «name»
+            this->AddEventSource(«name»);
+        '''
+    }
+
+}
